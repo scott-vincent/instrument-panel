5fe67d1bc98981c53b4a64336b9a71e4077cdfd3
Scott Vincent <scottvincent@yahoo.com>
2020-09-29 07:43:02 +0100

    wip

--- release.sh
+++ release.sh
@@ -5,6 +5,7 @@ mkdir release/$rel
 cp instrument-panel/instrument-panel release/$rel
 cp -rp instrument-panel/bitmaps release/$rel
 cp -rp instrument-panel/settings release/$rel
+sudo chown pi:pi instrument-panel/settings/*.json
 dos2unix instrument-panel/settings/default-settings.json
 cp instrument-panel/settings/default-settings.json instrument-panel/settings/instrument-panel.json
 tar -zcvf release/instrument-panel-$rel-raspi4.tar.gz release/$rel
