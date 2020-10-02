8bc6a9cfd617e1d752e9f5f0d66672de49df51d3
Scott Vincent <scottvincent@yahoo.com>
2020-10-02 18:42:51 +0100

    wip

--- instrument-panel/simvarDefs.cpp
+++ instrument-panel/simvarDefs.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "simvarDefs.h"
 
-const char* versionString = "v1.3.2";
+const char* versionString = "v1.3.3";
 
 const char* SimVarDefs[][2] = {
     { "Indicated Altitude", "feet" },
