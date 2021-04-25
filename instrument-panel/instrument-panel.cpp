c9106b2c5c9bb31ff86430254998f24ddb941f92
Scott Vincent <scottvincent@yahoo.com>
2021-04-25 09:03:18 +0100

    Fix FBW electrics check

--- instrument-panel/instrument-panel.cpp
+++ instrument-panel/instrument-panel.cpp
@@ -265,7 +265,14 @@ void updateCommon()
     SimVars* simVars = &globals.simVars->simVars;
 
     // Electrics check
-    globals.electrics = globals.connected && simVars->dcVolts > 0;
+    if (globals.aircraft == FBW_A320NEO) {
+        // Instruments don't come on if running on batteries only.
+        // Need external power, APU or main engines.
+        globals.electrics = globals.connected && simVars->dcVolts > 25.4;
+    }
+    else {
+        globals.electrics = globals.connected && simVars->dcVolts > 0;
+    }
 
     // Avionics check
     globals.avionics = globals.connected && (simVars->com1Status == 0 || simVars->com2Status == 0);
