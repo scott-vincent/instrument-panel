7c040290d4668c74ea7844602015b2c5165ce96c
Scott Vincent <scottvincent@yahoo.com>
2021-08-07 07:35:08 +0100

    Fix fuel flow bug

--- instrument-panel/instruments/egt.cpp
+++ instrument-panel/instruments/egt.cpp
@@ -137,7 +137,7 @@ void egt::update()
         egtRefAngle = egtAngle;
     }
 
-    if (loadedAircraft = FBW_A320NEO) {
+    if (loadedAircraft == FBW_A320NEO) {
         if (simVars->engineFuelFlow > 300) {
             flowAngle = 126 + (simVars->engineFuelFlow - 300) * 0.039;
         }
