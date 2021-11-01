3817c63f82c9ba2e4250ebb9744af71c68dabd49
Scott Vincent <scottvincent@yahoo.com>
2021-11-01 10:50:43 +0000

    Update Allegro to 5.2.7

--- instrument-panel/instrument-panel.cpp
+++ instrument-panel/instrument-panel.cpp
@@ -10,14 +10,17 @@
  *
  * It has been completely rewritten and updated to use Allegro5.
  *
- * NOTE: Allegro5 must be built on RasPi4 as a standard Linux build, not the
- * specific Raspberry Pi build, i.e.:
+ * To install Allegro (5.2.7) on Windows:
  *
- *     mkdir build
- *     cd allegro5/build
- *     cmake .. -DCMAKE_BUILD_TYPE=Release
- *     make && sudo make install
- * 
+ *     Load instrument-panel.sln into Visual Studio Community Edition.
+ *     When you build the solution it will automatically download Allegro
+ *     as it is specified in packages.config. A new folder called
+ *     packages will be created under the main folder.
+ *
+ * To install Allegro on Raspberry Pi:
+ *
+ *     apt install liballegro5-dev
+ *
  * KEYS
  * 
  * p ........ Adjust position and size of individual instruments.
