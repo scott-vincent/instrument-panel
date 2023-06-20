d5cea61fc8e18a5c126708ec70c5326b48261e6b
Scott Vincent <scottvincent@yahoo.com>
2023-06-20 06:37:05 +0100

    New simvar

--- instrument-panel/simvarDefs.cpp
+++ instrument-panel/simvarDefs.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "simvarDefs.h"
 
-const char* versionString = "v1.7.6";
+const char* versionString = "v1.7.7";
 
 const char* SimVarDefs[][2] = {
     // Vars for Jetbridge (must come first)
