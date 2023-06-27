ab440a4f12e459545269e5735b6eb2f12f8c0c6c
Scott Vincent <scottvincent@yahoo.com>
2023-06-27 16:31:35 +0100

    Minor tweaks

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
 cd instrument-panel
+if [ -f /usr/local/bin/fs-flip.dat ]
+then
+  DISPLAY=:0 xrandr --output HDMI-1 --rotate inverted
+else
+  DISPLAY=:0 xrandr --output HDMI-1 --rotate normal
+fi
 ./instrument-panel
